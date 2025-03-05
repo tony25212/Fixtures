dad4ecb7fcfaffe0668732692d7f146f39518f08
tony25212 <tony.401@btinternet.com>
2025-03-05 14:14:50 +0000

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -1,6 +1,5 @@
 import time
 from datetime import datetime
-import driver
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,43 +10,46 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.edge.options import Options
 import csv
 import os
-
+import streamlit as st
 
 # Define driver, options and service
-#edge_options = Options()
-#edge_options.add_argument("--disable-search-engine- choice-screen")
 URL = 'https://www.rfyl.org.uk/core/clubs.aspx'
-#URL = "https://bbc.com"
-# Define the pattern to search for the executable
 edge_driver_path = "Web_driver/msedgedriver.exe"
-#os.chmod(edge_driver_path, 0o755)
+
+# Set execute permission for msedgedriver.exe
+os.chmod(edge_driver_path, 0o755)
+
 # Set up the Edge service
-service = Service(edge_driver_path)  # No need to specify the path if msedgedriver is in your PATH
+service = Service(edge_driver_path)
+
 # Set up Edge options
 options = Options()
 options.add_argument("--headless")  # Ensure GUI is off
 options.add_argument("--disable-gpu")
+
 # Launch Edge with the specified options
 driver = webdriver.Edge(service=service, options=options)
+
 # Load website
 driver.get(URL)
 
 """
-Loging in to a site
+Logging in to a site
 
 # Locate the needed fields
 username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
-button_field = driver.find_element(By.ID, 'login')# to prevent adds being clicked
+button_field = driver.find_element(By.ID, 'login')  # to prevent ads being clicked
 
 # Fill in the data and action
 username_field.send_keys('the user name')
 password_field.send_keys('the PW')
 driver.execute_script("arguments[0].click();", button_field)
 """
+
 def scrape_data():
     # Locate and update the competition field
-    competition_field = WebDriverWait(driver, 0).until(EC.visibility_of_element_located((By.ID, 'ddlCompetitions')))
+    competition_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'ddlCompetitions')))
     Select(competition_field).select_by_visible_text('Under 12 9v9 Division 10')
 
     # Wait for the second field to be refreshed
@@ -57,21 +59,27 @@ def scrape_data():
     club_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'ddlClubs')))
     Select(club_field).select_by_visible_text('CONSETT WARRIORS')
 
-    #Pause for page refresh
+    # Pause for page refresh
     time.sleep(1)
 
     # Wait for the table to be refreshed the first time
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//table[@id='gvClubFixtures']/tbody/tr")))
+    
     # Get the page source
     page_source = driver.page_source
+    
     # Close the WebDriver
     driver.quit()
+    
     # Parse the page source with BeautifulSoup
     soup = BeautifulSoup(page_source, 'html.parser')
+    
     # Locate the table
     table = soup.find('table', {'id': 'gvClubFixtures'})
+    
     # Read the rows of the table
     rows = table.find_all('tr')
+    
     # Loop through the rows and print the data
     filename = "output.csv"
 
@@ -86,13 +94,14 @@ def scrape_data():
             cells = row.find_all('td')
             if len(cells) > 2:  # Ensure there are at least 3 cells in the row
                 date_object = datetime.strptime(cells[0].text, '%d/%m/%y %H:%M')
-               # Get the weekday (0=Monday, 6=Sunday)
+                # Get the weekday (0=Monday, 6=Sunday)
                 weekday = date_object.weekday()
                 # Convert the weekday number to a name
                 weekday_name = date_object.strftime('%A')
                 fixtures = [cells[0].text, weekday_name, cells[1].text, cells[2].text, cells[4].text]
                 csvwriter.writerow(fixtures)
     return True
+
 if __name__ == '__main__':
     get_data = scrape_data()
     print(get_data)
